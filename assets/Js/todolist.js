90775d69cb9bfac9fea83c4849e5a40f685a0273
arthurborges <arthurborges.senai564@gmail.com>
2023-06-12 14:15:50 -0300

    todolist js fixed

--- assets/Js/todolist.js
+++ assets/Js/todolist.js
@@ -3,6 +3,7 @@ let aresu = [];
 let adat = [];
 let aautor = [];
 let acateg = [];
+let cssI = 0
 
 function sendN() {
     let title = document.getElementById("title").value;
@@ -11,26 +12,28 @@ function sendN() {
     let autor = document.getElementById("autor").value;
     let categ = document.getElementById("categories").value;
     let datfix = dat.split("-").reverse().join("/");
-    atitle.push(title);
-    aresu.push(resu);
-    adat.push(datfix);
-    aautor.push(autor);
-    acateg.push(categ);
+   
+
+    
 
     if(title && resu && dat && autor && categ){
-
-    for (let cssI = 0; cssI < atitle.length; cssI++) {
-        let divtitle = '<section id="newssec"' + cssI + '"><h1>' + atitle[cssI] + '</h1>';
+        atitle.push(title);
+        aresu.push(resu);
+        adat.push(datfix);
+        aautor.push(autor);
+        acateg.push(categ);
+         
+        let divtitle = '<section id="newssec' + cssI + '"><h1>' + atitle[cssI] + '</h1>';
         let divresu = '<h2 class="resums">' + aresu[cssI] + '</h2>';
         let divcateg = '<p class="categs">' + 'Categoria: ' + acateg[cssI] + '</p>';
         let divautor = '<p class="autors">' + 'Escrito por: ' + aautor[cssI] + '</p>';
         let divdat = '<p class="dates">' + adat[cssI] + '</p>';
         let editbtn = '<button class="editbtn" onclick="editN(' + cssI + ')">Editar</button>';
-        let removebtn = '<button class="editbtn" onclick="removeN(' + cssI + ')">Remover</button> </section>';
+        let removebtn = '<button class="editbtn" onclick="removeN('+ cssI +')">Remover</button> </section>';
         let newsContainer = document.getElementsByClassName("news")[0];
         newsContainer.innerHTML += divtitle + divresu + divcateg + divautor + divdat + editbtn + removebtn;
        
-    }
+       
 
     document.getElementById("title").value = "";
     document.getElementById("resume").value = "";
@@ -40,9 +43,11 @@ function sendN() {
     document.getElementById("categories").value = "";
     
     }
+    cssI++;
 }
 
 function removeN(cssInone) {
+   
     let sectionId = "newssec" + cssInone;
     let section = document.getElementById(sectionId);
     section.style.display = "none";
@@ -55,6 +60,8 @@ function editN() {
     let categ = document.getElementById("categories").value;
     let datfix = dat.split("-").join("/");
 }
+
+var i = 0
 
 
 
